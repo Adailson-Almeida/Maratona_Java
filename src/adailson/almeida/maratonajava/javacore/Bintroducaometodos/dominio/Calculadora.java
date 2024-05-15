31cb6ab8dd04b0cacc247edb05324a13c7b9ee6c
Adailson-Almeida <adailson.almeida@outlook.com.br>
2024-05-15 14:26:41 -0300

    outra implemetação de métodos

--- src/adailson/almeida/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/adailson/almeida/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -20,4 +20,12 @@ public class Calculadora {
     public double divideDoisNumeros(double num1, double num2){
         return (num1 / num2);
     }
+
+    public void imprimeDivisaoDoisNumeros(double num1, double num2) {
+        if (num2 == 0){
+            System.out.println("Não existe divisão por zero");
+            return;
+        }
+        System.out.println(num1 / num2);
+    }
 }
