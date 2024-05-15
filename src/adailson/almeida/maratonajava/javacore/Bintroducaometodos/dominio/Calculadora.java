2817b2ac6b1a233f614e123b2b4836898dc44023
Adailson-Almeida <adailson.almeida@outlook.com.br>
2024-05-15 13:25:17 -0300

    atualização metódos

--- src/adailson/almeida/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/adailson/almeida/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -3,14 +3,21 @@ package adailson.almeida.maratonajava.javacore.Bintroducaometodos.dominio;
 public class Calculadora {
 
     public void somaDoisNumeros() {
+
         System.out.println(10 + 10);
     }
 
     public void subtracaoDoisNumeros() {
+
         System.out.println(20 - 10);
     }
 
     public void multiplicaDoisNumeros(int num1, int num2) {
+
         System.out.println(num1 * num2);
+    }
+
+    public double divideDoisNumeros(double num1, double num2){
+        return (num1 / num2);
     }
 }
